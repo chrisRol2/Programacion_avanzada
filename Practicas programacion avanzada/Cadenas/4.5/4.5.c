84f4728194a7e1af3f834536a0381108a99e4ee1
chrisRol2 <Christopher_rol@hotmail.es>
2020-09-15 16:50:03 -0300

    15092020

--- Practicas programacion avanzada/Cadenas/4.5/4.5.c
+++ Practicas programacion avanzada/Cadenas/4.5/4.5.c
@@ -10,14 +10,14 @@
  *	Docente:        Iervasi Scokin, Juan Jose
  *
  *	Ejercicio:  5.	Hacer una función que permita insertar en una secuencia de dígitos 
-                        los caracteres guión cada 8 dígitos a partir del cuarto dígito, 
-                        y un espacio cada 8 dígitos a partir del octavo dígito.
-                        Si la cantidad de dígitos no es múltiplo de ocho agregar delante
-                        de la cadena caracteres ‘0’ hasta que la misma sea múltiplo de 8.
-
-	Ejemplos:	472348237388539055823012	->	4723-4823 7388-5390 5582-3012
-		8237388539055823012		->	0000-0823 7388-5390 5582-3012
-
+ *                      los caracteres guión cada 8 dígitos a partir del cuarto dígito, 
+ *                      y un espacio cada 8 dígitos a partir del octavo dígito.
+ *                      Si la cantidad de dígitos no es múltiplo de ocho agregar delante
+ *                      de la cadena caracteres ‘0’ hasta que la misma sea múltiplo de 8.
+ *
+ *	Ejemplos:	472348237388539055823012	->	4723-4823 7388-5390 5582-3012
+ *		8237388539055823012		->	0000-0823 7388-5390 5582-3012
+ *
  *
  *
  *
@@ -30,8 +30,9 @@
 
  // Declaro funciones
  void insertarGuion(char cadena[200]);
- void concatenar(char cadena1[],char cadena2[]);
- void concatenarDer(char cadena1[],char* cadena2);
+ void concatenar(char origen,char destino[]);
+ void concatenarDer(char destino[], char *orige);
+ int strlen(char cadena[]);
 int main(void) {
     char cadena[200];
     printf("Ingresar cadena: "); scanf("%s", cadena);
@@ -49,7 +50,7 @@ void insertarGuion(char cadena[200]) {
  int guion = 3, espacio = 7;
  char aux[200]="";
     while( resto != 0 ) {
-        concatenar("0",cadena);
+        concatenar('0',cadena);
         resto = strlen(cadena) % 8;
     }
     for ( int i = 0; i < strlen(cadena); i++ ) {
@@ -63,16 +64,47 @@ void insertarGuion(char cadena[200]) {
     for( int i = 0; i < strlen(aux); i++ )cadena[i] = aux[i];
 }
 
-void concatenarDer(char cadena2[], char *cadena1) {
+void concatenarDer(char destino[], char *origen) {
 //    printf("\ncaracter1: %c\tcadena2: %s", *cadena1, cadena2);
-    sprintf(cadena2, "%s%c", cadena2, *cadena1);
+    //sprintf(cadena2, "%s%c", cadena2, *cadena1);
+	int pos = 0;
+	while( destino[pos] != '\0' ) {
+		pos++;
+	}
+	destino[pos] = *origen;
+	destino[pos+1] = '\0';
 
 }
 
-
-void concatenar(char cadena1[], char cadena2[]) {
+void concatenar(char origen, char destino[]) {
   //  printf("\ncadena1: %s\ncadena2: %s\n", cadena1, cadena2);
-    char aux[200];
-    sprintf(aux, "%s", cadena2);
-    sprintf(cadena2, "%s%s", cadena1, aux);
+	int i;
+	char aux[200]=" ";
+	/*for( i = 0; destino[i - 1] != '\0'; i++ ) {
+		aux[i] = destino[i];
+	}
+	destino[0] = '0';
+	for( i = 1; aux[i-1] != '\0'; i++ ) {
+		destino[i] = aux[i-1];
+	}
+	destino[i] = '\0';
+	*/
+	for( i = strlen(destino) + 1; i >= 0; i-- ) {
+		destino[i + 1] = destino[i];
+	}
+	destino[0] = origen;
+
+	printf("cadena2: %s", destino);
+	
+	
+	//sprintf(destino, "%c%s", origen, aux);
+	
+}
+
+int strlen(char cadena[]){
+	int length = 0;
+	while( cadena[length] != '\0' ) {
+		length++;
+	}
+	return length;
 }
