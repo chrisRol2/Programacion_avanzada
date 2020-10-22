cc09cd5770557a00476ba125ce0c0de45c4f4d3b
Chris Rol <christopher_rol@hotmail.es>
2020-10-22 00:27:08 -0300

    22102020

--- Practicas programacion avanzada/ArchivosBinarios/6.1/6.1.c
+++ Practicas programacion avanzada/ArchivosBinarios/6.1/6.1.c
@@ -23,26 +23,42 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <locale.h>
 
 #define SIZE 32
-#define FILENAME "archivo.bin"
+#define FILENAME "archivo"
 // Declaro funciones
 int grabarArch(char nombre[SIZE]);
 int mostrarArch(char nombre[SIZE]);
 int agregarAcumulado(char nombre[SIZE]);
-int ordenarArch(char nombre[SIZE]);
 int buscarArch(char nombre[SIZE], int buscado);
+int ordenarArch(char nombre[SIZE]);
 void eliminarValor(char nombre[SIZE], int buscado);
 void eliminarValor1(char nombre[SIZE], int buscado);
 
+void ingresarDato(int *dato, int min, int max);
 int random(int minimo, int maximo);
+void ordenar    (int vec[], int n);
+void imprimir(int vec[], int n);
+
 
 int main(void) {
+	int buscado;
 	srand(getpid());
 	if( grabarArch(FILENAME) ) return -1;
 	mostrarArch(FILENAME);
-	agregarAcumulado(FILENAME);
-	mostrarArch(FILENAME);
+	printf("Buscado: ");
+	ingresarDato(&buscado, 0, 999);
+	printf("\tEl %d esta en la pos: %d\n",
+		   buscado,
+		   buscarArch(FILENAME, buscado)+1
+	);
+	ordenarArch(FILENAME);
+	eliminarValor(FILENAME, buscado);
+	printf("Buscado 2: ");
+	ingresarDato(&buscado, 0, 999);
+	eliminarValor1(FILENAME, buscado);
 
 	printf("\n"); system("PAUSE");
 	return 0;
@@ -61,6 +77,8 @@ int grabarArch(char nombre[SIZE]){
 		fwrite(&entero, sizeof(int), 1, archivo);
 	}
 	fclose(archivo);
+
+	agregarAcumulado(FILENAME);
 }
 int mostrarArch(char nombre[SIZE]){
 	int entero=0;
@@ -96,12 +114,115 @@ int agregarAcumulado(char nombre[SIZE]){
 	fwrite(&suma, sizeof(int), 1, archivo);
 	fclose(archivo);
 }
-int ordenarArch(char nombre[SIZE]){}
-int buscarArch(char nombre[SIZE], int buscado){}
-void eliminarValor(char nombre[SIZE], int buscado){}
-void eliminarValor1(char nombre[SIZE], int buscado){}
+int buscarArch(char nombre[SIZE], int buscado){
+	FILE *archivo;
+	int aux = NULL;
+	archivo = fopen(nombre, "rb");
+	if( archivo == NULL ) {
+		puts("No se pudo abrir el archivo");
+		return -1;
+	}
+	for( int i = 0; i < 10; i++ ) {
+		fread(&aux, sizeof(int), 1, archivo);
+		if( aux == buscado) {
+			return i;
+		}
+	}
+	return -2;
+}
+int ordenarArch(char nombre[SIZE]){
+	int aux[10];
+	FILE *archivo;
+	archivo = fopen(nombre, "rb+");
+	if( archivo == NULL ) {
+		puts("No se pudo abrir el archivo");
+		return 0;
+	}
+	fread(aux, sizeof(int), 10, archivo);
+	ordenar(aux, 10);
+	fseek(archivo, 0, SEEK_SET);
+	fwrite(aux, sizeof(int),10, archivo);
+	fclose(archivo);
+	mostrarArch(FILENAME);
+	return 1;
+}
+void eliminarValor(char nombre[SIZE], int buscado){
+	FILE *archivo;
+	int aux[10];
+
+	archivo = fopen(nombre, "rb+");
+	if( archivo == NULL ) {
+		puts("No se pudo abrir el archivo");
+		return -1;
+	}
+	fread(aux, sizeof(int), 10, archivo);
+	for( int i = 0; i < 10; i++ ) {
+		if( aux[i] == buscado ) {
+			aux[i] = -1;
+		}
+	}
+	fseek(archivo, 0, SEEK_SET);
+	fwrite(aux, sizeof(int),10, archivo);
+	fclose(archivo);
+	mostrarArch(FILENAME);
+
+}
+void eliminarValor1(char nombre[SIZE], int buscado){
+	FILE *archivo;
+	FILE *temp;
+	int aux[11];
+
+	archivo = fopen(nombre, "rb+");
+
+	if( archivo == NULL ) {
+		puts("No se pudo abrir el archivo");
+		return ;
+	}
+
+	fread(aux, sizeof(int), 11, archivo);
+	buscado = buscarArch(FILENAME, buscado);
+	for( int i = buscado; i < 10; i++ ) {
+		aux[i] = aux[i+1];
+	}
+	//imprimir(aux, 10);
+	
+	
+	fclose(archivo);
+	rename("archivo","archivo32.bin");
+	//mostrarArch(FILENAME);
+	//remove(FILENAME);
+
+}
 
 int random(int minimo, int maximo) {
 
 	return rand() % (maximo - minimo + 1) + minimo;
+}
+void ingresarDato(int *dato, int min, int max) {
+
+	do {
+		scanf("%d", dato);
+		if( (*dato > max) || (*dato < min) )printf("\nDato fuera de rango.\n");
+	} while( (*dato > max) || (*dato < min) );
+
+}
+void ordenar(int vec[], int n) {
+
+	int aux;
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (vec[i] > vec[j]) {
+				aux = vec[i];
+				vec[i] = vec[j];
+				vec[j] = aux;
+			}
+		}
+	}
+
+}
+void imprimir(int vec[], int n) {
+
+
+	for (int i = 0; i < n; i++)printf("%d\n", vec[i]);
+
 }
--- Practicas programacion avanzada/Utilidades/test/test.c
+++ Practicas programacion avanzada/Utilidades/test/test.c
@@ -1,196 +1,33 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-typedef struct sfecha {??
-int dia, mes, anio;
-}??tfecha;
-
-
-
-typedef struct sproducto {??
-
-
-
-int codbarras[21];
-
-char nombre[60];
-
-float precio;
-
-int stock;
-
-tfecha fecha;
-
-
-
-}??tprod;
-
-
-
-void crearArchivo(char nombreA[]);
-
-
-
-int main(void)
-
-{??
-
-char nombreArch[] = "prueba1.bin";
-
-
-
-// 1) Creo la variable para manejar el archivo
-
-FILE * archivoC;
-
-
-
-tfecha t1;
-
-
-
-// funcion para crear el archivo
-
-crearArchivo(nombreArch);
-
-
-
-
-
-// 2) Abrir el archivo (en este caso lo abro para lectura (r) y es binario (b)
-
-archivoC = fopen(nombreArch, "rb");
-
-if (archivoC == NULL)
-
-{??
-
-printf("no se puede crear el archivo");
-
-return 1;
-
-}??
-
-
-
-// Proceso el archivo
-
-
-
-
-
-// feof: es una funcion (que viene con C) y me devuelve verdadero
-
-// cuando el archivo termino (cuando tengo que parar de leer)
-
-// usando la negacion de foef (!feof) voy a entrar al while
-
-// mientras que el archivo no haya terminado
-
-//feof necesita una lectura previa de fread
-
-fread(&t1, sizeof(tfecha), 1, archivoC);
-
-while(!feof(archivoC))
-
-{??
-
-if (t1.dia>6)
-
-{??
-
-printf("Dia: %d\n", t1.dia);
-
-}??
-
-
-
-fread(&t1, sizeof(tfecha), 1, archivoC);
-
-}??
-
-
-
-// Cierro el archivo
-
-fclose(archivoC);
-
-
-
-
-
-system("pause");
-
-return 0;
-
-
-
-
-
-}??
-
-
-
-void crearArchivo(char nombreA[])
-
-{??
-
-tfecha t1;
-
-
-
-// 1) Creo la variable para manejar el archivo
-
-FILE * archivo1;
-
-
-
-// 2) Abrir el archivo (en este caso lo abro para escritura (w) y es binario (b)
-
-archivo1 = fopen(nombreA, "wb");
-
-if (archivo1 == NULL)
-
-{??
-
-puts("no se puede crear el archivo");
-
-return 1;
-
-}??
-
-
-
-printf("Ingrese Dia ? (-1 para terminar)");
-
-scanf("%d", &t1.dia);
-
-while (t1.dia != -1)
-
-{??
-
-t1.mes = 3; t1.anio = 2020;
-
-fwrite(&t1, sizeof(tfecha), 1, archivo1);
-
-printf("Ingrese Dia ? (-1 para terminar)");
-
-scanf("%d", &t1.dia);
-
-}??
-
-
-
-// 4) Cierro el archivo
-
-fclose(archivo1);
-
-}??
-
-
-int selectorLetra = random(); // este random debe dar 0 o 1
-if( selectorLetra )
-  dato.TipoDePozo = 'G';
-else 
-  dato.TipoDePozo = 'P';
+﻿char eliminacionFisica()
+{
+    FILE *archivo;
+    FILE *temporal;
+    Producto producto;
+    char elimina = 0;
+
+    archivo = fopen("productos.dat", "rb");
+    temporal = fopen("temporal.dat", "wb");
+
+    if (archivo == NULL || temporal == NULL) {
+        elimina = 0;
+    } else {
+        /* Se copia en el archivo temporal los registros válidos */
+        fread(&producto, sizeof(producto), 1, archivo);
+        while (!feof(archivo)) {
+            if (producto.codigo != VALOR_CENTINELA) {
+                fwrite(&producto, sizeof(producto), 1, temporal);
+            }
+            fread(&producto, sizeof(producto), 1, archivo);
+        }
+        /* Se cierran los archivos antes de borrar y renombrar */
+        fclose(archivo);
+        fclose(temporal);
+
+        remove("productos.dat");
+        rename("temporal.dat", "productos.dat");
+
+        elimina = 1;
+    }
+
+    return elimina;
+}
