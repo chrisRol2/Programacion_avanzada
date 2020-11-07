7a55c2d6ff07da231a0bd254f56df392872aaeb2
Chris Rol <christopher_rol@hotmail.es>
2020-11-07 14:45:05 -0300

    [07112020] -> practica parcial y test

--- Practicas programacion avanzada/Utilidades/test/test.c
+++ Practicas programacion avanzada/Utilidades/test/test.c
@@ -1,151 +1,181 @@
-﻿/*
-•	Crear el archivo, ingresando los datos por teclado.
-•	Borrar del archivo los registros de aquellos lotes que se encuentren vencidos, comparando la fecha de vencimiento de cada uno con una fecha testigo que se ingresa por teclado. Para borrar los registros es necesario generar un nuevo archivo que contenga solamente los lotes no vencidos, emitiendo además un listado por pantalla con los registros eliminados.
-•	Generar e imprimir un listado con el código, el nombre y la cantidad de unidades en stock para cada medicamento, sin importar el lote al que pertenezca. El archivo no está ordenado, y los medicamentos son a lo sumo 130. 
-*/
-
-#define _CRT_SECURE_NO_WARNINGS
-
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
 #include <stdlib.h>
-
+#include <string.h>
 #include <time.h>
 
+#define MAX 10
 
-void crearArchivo(char nombreA[]);
-void mostrarArchivo(char nombreA[]);
-void ordenarBurbujaVectores(char nombreA[]);
-int leerregistro(FILE * f, int numreg);
-void grabarregistro(FILE * f, int numreg, int dato);
 
-int main(void)
+//ESTRUCTURAS
+typedef struct pasajes
 {
-	char nombreArch[] = "prueba1.bin";
+    int CI, edad, pago, precio, mes;
+    char apellido[3];
+    char asiento;
+}tpasaje;
 
-	// funcion para crear el archivo
-	crearArchivo(nombreArch);
+//PROTOTIPOS
+void CargarArch(char archO[]);
+void MostrarPantalla(char archO[]);
+void buscar(char archO[]);
 
-	// funcion para mostrar el archivo
-	mostrarArchivo(nombreArch);
-
-	// lo ordeno
-	ordenarBurbujaVectores(nombreArch);
-
-	// funcion para mostrar el archivo
-	mostrarArchivo(nombreArch);
-
-	system("pause");
-	return 0;
-
-
+//PROGRAMA PRINCIPAL
+int main()
+{
+    int cant;
+    srand(time(NULL));
+    tpasaje aux1;
+    char ArchivoBinario[]="Pasajes.bin";
+    char Archivo2Binario[]="Vendidos.bin";
+    char ArchivoReporte[]="Reporte.txt";
+    CargarArch(ArchivoBinario);
+    MostrarPantalla(ArchivoBinario);
+    buscar(ArchivoBinario);
+    system("pause");
+    return 0;
 }
 
-void ordenarBurbujaVectores(char nombreA[])
+
+void CargarArch(char archO[])
 {
-	FILE* arch;
-	int cantreg;
-	int dato1;
-	int dato2;
-	int i, j;
-
-	// si lo abro con rb+, el archivo tiene que existir y puedo leerlo y escribirlo
-	arch=fopen(nombreA, "rb+");
-	if (arch==NULL){
-		puts("Error");
-
-	}
-
-	// Dos pasos para obtener la cantidad de registros totales del archivo
-	fseek(arch, 0, SEEK_END); //ubico indicador al final
-	cantreg = ftell(arch)/sizeof(int); //obtengo cantidad de reg.
-
-									   //Metodo de burbuja
-	for(i=0;i<cantreg-1;i++)
-	{
-		for (j=i+1;j<cantreg;j++)
-		{
-			tuestructura1=leerregistro(arch,i);
-			tuestructura2=leerregistro(arch,j);
-			if (tuestructura1.dato>tuestructura2.dato)
-			{
-				grabarregistro(arch,j,tuestructura1);
-				grabarregistro(arch,i,tuestructura2);
-			}
-		}
-	}
-
-	fclose(arch);
-
+    int cant = 0;
+    int i,k;
+    int azar;
+    tpasaje aux;
+    FILE* archivoB;
+    archivoB= fopen(archO, "wb");
+    if (archivoB == NULL)
+    {
+        printf("No se puede crear el archivo\n");
+        return 1;
+    }
+    aux.CI=rand()%(900000-100000+1)+100000;
+    while(cant<MAX)
+    {
+        for(k=0;k<3;k++)
+        {
+            aux.apellido[k] = '0' + rand() % (42-17+1) + 17;
+        }
+        aux.apellido[2]='\0';
+        azar= rand()%(100-1+1)+1;
+        if(azar<33)
+        {
+            aux.asiento='V';
+        }
+        else if(azar<66)
+        {
+            aux.asiento='M';
+        }
+        else
+        {
+            aux.asiento='P';
+        }
+        aux.edad= rand()%(75-18+1)+18;
+        aux.pago= rand()%(2-1+1)+1;
+        if (aux.pago==1)
+        {
+            aux.precio= rand()%(120000-30000+1)+30000;
+        }
+        else
+        {
+            aux.precio=  rand()%(150000-50000+1)+50000;
+        }
+        aux.mes=rand()%(12-1+1)+1;
+        fwrite(&aux, sizeof(tpasaje), 1, archivoB);
+        cant++;
+    }
+    fclose(archivoB);
 }
 
-int leerregistro(FILE * f, int numreg)
+
+
+void MostrarPantalla(char archO[])
 {
-	int dato;
-	// con fseek me posiciono en el numero de registro que me piden
-	fseek(f, numreg * sizeof(int),SEEK_SET);
-	// una vez posicionado, leo con fread
-	fread(&dato, sizeof(int), 1, f);
-	return dato;
+    FILE* archivoB;
+    tpasaje aux;
+    archivoB = fopen(archO, "rb");
+    if (archivoB == NULL)
+    {
+        printf("No se puede abrir el archivo\n");
+        return 1;
+    }
+    else
+    {
+        printf("CI\tNom.\tEdad\tAsiento\tMod.\tPrecio\tMes\n");
+        fread(&aux,sizeof(tpasaje),1,archivoB);
+        while (!feof(archivoB))
+        {
+            printf("%d\t", aux.CI);
+            printf("%s\t", aux.apellido);
+            printf("%d\t", aux.edad);
+            printf("%c\t", aux.asiento);
+            printf("%d\t", aux.pago);
+            printf("%d\t", aux.precio);
+            printf("%d\n", aux.mes);
+            fread(&aux, sizeof(tpasaje),1, archivoB);
+        }
+    }
+    fclose(archivoB);
 }
 
-void grabarregistro(FILE * f, int numreg, int dato)
+
+void buscar(char archO[])
 {
-	// con fseek me posiciono en el numero de registro que me piden
-	fseek(f, numreg * sizeof(int),SEEK_SET);
-	// una vez posicionado, escribo con fwrite
-	fwrite(&dato,sizeof(int),1,f);
+    tpasaje aux;
+    FILE*archivoB;
+    char dato[3];
+    printf("\nINGRESE UN APELLIDO PARA BUSCAR SUS DATOS: ");
+    gets(dato);
+    archivoB=fopen(archO,"rb");
+    if(archivoB==NULL)
+    {
+        printf("No se puede abrir el archivo\n");
+        return 1;
+    }
+    fread(&aux, sizeof(tpasaje), 1, archivoB);
+    while( !feof(archivoB) )
+    {
+        if( strcmp(dato, aux.apellido) == 0 )
+        {
+
+            printf("CI\tEdad\tAsiento\tMod.\tPrecio\tMes\n");
+            printf("%d\t", aux.CI);
+            printf("%d\t", aux.edad);
+            printf("%c\t", aux.asiento);
+            printf("%d\t", aux.pago);
+            printf("%d\t", aux.precio);
+            printf("%d\n", aux.mes);
+        }
+        fread(&aux, sizeof(tpasaje), 1, archivoB);
+    }
+    fclose(archivoB);
 }
 
-void mostrarArchivo(char nombreA[])
-{
-	// 1) Creo la variable para manejar el archivo
-	FILE * archivoC;
-	int aux;
-
-	// 2) Abrir el archivo (en este caso lo abro para lectura (r) y es binario (b)
-	archivoC = fopen(nombreA, "rb");
-	if (archivoC == NULL)
-	{
-		printf("no se puede crear el archivo");
-		return 1;
-	}
-
-	// 3) Proceso el archivo
-
-	fread(&aux, sizeof(int), 1, archivoC);
-	while(!feof(archivoC))
-	{
-		printf("Entero: %d\n", aux);
-		fread(&aux, sizeof(int), 1, archivoC);
-	}
-
-	// Cierro el archivo
-	fclose(archivoC);
-}
-
-void crearArchivo(char nombreA[])
-{
-	int i;
-	int aux;
-
-	// 1) Creo la variable para manejar el archivo
-	FILE * archivo1;
-
-	// 2) Abrir el archivo (en este caso lo abro para escritura (w) y es binario (b)
-	archivo1 = fopen(nombreA, "wb");
-	if (archivo1 == NULL)
-	{
-		puts("no se puede crear el archivo");
-		return 1;
-	}
-
-	for (i = 0; i < 100; i++)
-	{
-		aux = rand()%(1000);
-		fwrite(&aux, sizeof(int), 1, archivo1);
-	}
-
-	// 4) Cierro el archivo
-	fclose(archivo1);
-}
+/*• Código de identificación (entero, entre 100000 y 900000)
+• Tipo de asiento (1 caracter, V: ventanilla, P: pasillo, M: medio)
+• Apellido comprador (3 caracteres)
+· Edad comprador (entero, entre 18 y 75)
+· Modalidad de pago (entero, 1: contado, 2: crédito)
+· Precio del pasaje (entero, entre 30000 y 120000 si es pago contado y entre 50000 y 150000 si es pago a credito)
+· Mes de venta (1 entero, entre 1 y 12)*/
+typedef struct {
+    int codID;
+    char tipoAsiento;
+    char apellido[4];
+    int edad;
+    char modPago;
+    int precio;
+    int mesVenta;
+}pasaje;
+/*
+1. Genere, sin intervención del usuario, un archivo binario de pasajes vendidos en un mes (considere una cantidad de 100 pasajes). (1Punto).
+2. Genere una función que muestre los datos guardados en el archivo de pasajes (1Punto)
+3. Genere una función que, a partir de un apellido ingresado por el usuario, muestre los datos del pasaje que compró. (2Puntos)
+4. Ordene los pasajes vendidos por precio, luego imprima nuevamente el archivo (2Punto).
+5. Genere un nuevo archivo que contenga sólo las ventas realizadas en las temporadas otoño-invierno (meses 4 al 8). Muestre los datos guardados en este nuevo archivo (2Puntos).
+6. Genere un archivo de texto con el siguiente formato (2Puntos):
+    Apellido Edad Tipo de Pago Precio
+    BH 50 1 18500
+    IK 23 2 20300
+    */
